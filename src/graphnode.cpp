4c51021d9f0e35997d28a62dec228ccd45355d47
chengfang <chengfang.kid@gmail.com>
2020-04-03 18:35:20 +0200

    first commit

---
+++ src/graphnode.cpp
@@ -0,0 +1,58 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    // fix bug, this lead to delete chatBot twice
+    //delete _chatBot; 
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token) {
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge) {
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge) {
+    _childEdges.push_back(std::unique_ptr<GraphEdge>(edge));
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot &&chatBot) {
+    _chatBot = std::move(chatBot);
+
+    // immediately update the _chatBot handle inside ChatLogic
+    _chatBot.GetChatLogicHandle()->SetChatbotHandle(&_chatBot);
+    _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode) {
+    newNode->MoveChatbotHere(std::move(_chatBot));
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
